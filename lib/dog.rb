7f5e8c4d116db10aa5f3c6d07fac6dc2d2299085
hykeung92 <hykeung92@gmail.com>
2020-04-21 16:35:58 -0700

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+Class Dog
+    attr_accessor :id, :name, :breed
+
+    def initialize(attributes)
+        attributes.each {|key, value| self.send(("#{key}="), value)}
+        self.id ||= nil
+    end
+
+    def self.create_table
+        sql = <<-SQL
+            CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT
+            )
+        SQL
+        DB[:conn].execute(sql)
+    end
+
+end
